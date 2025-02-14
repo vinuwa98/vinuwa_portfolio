323e1ab2eb13fc1a445f06924d8c2831133e2f24
vinod warnakulasooriya <116092558+vinuwa98@users.noreply.github.com>
2025-02-14 22:44:05 +0530

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -43,7 +43,20 @@ const Hero = () => {
         className="stars"
         animate={controls}
       ></motion.div>
-      
+      <div className="wrapper">
+        <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
+          <motion.h2 variants={textVariants}>Aspiring Software Engineer</motion.h2>
+          <motion.h1 variants={textVariants}>
+          Tech Enthusiast <br /> and <br /> Information Technology Tutor
+          </motion.h1>
+
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>See the latest Work</motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+          <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="scroll" />
+        </motion.div>
+      </div>
 
       <motion.div className="slidingTextContainer" variants={sliderVariants} initial="initial" animate="animate">
         Coder Gamer Guitarist ScienceLover Tutor
